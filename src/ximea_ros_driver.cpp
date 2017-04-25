a2623e5a8360c8fbeb09762e5fbb0fd31e96828a
geeteshdubey <geeteshdubey@gmail.com>
2017-04-25 03:06:57 -0400

    fixed driver bug

--- src/ximea_ros_driver.cpp
+++ src/ximea_ros_driver.cpp
@@ -66,6 +66,7 @@ void ximea_ros_driver::publishCamInfo(const ros::Time &now)
 {
   ros_image_.header.stamp = now;
   cam_info_ = cam_info_manager_->getCameraInfo();
+  cam_info_.header.stamp = now;
   cam_info_pub_.publish(cam_info_);
 }
 
